41cbb8aa471a15067250f6ce950b346e818910e3
ashkanhpb <ashkanhpb@gmail.com>
2024-06-26 18:32:55 +0330

    change information transfer  by render

--- website/views.py
+++ website/views.py
@@ -6,7 +6,8 @@ def index_view(request):
 def contact_view(request):
     return render(request,'website/contact.html')
 def about_view(request):
-    return render(request,'website/about.html')
+    context={"birthday":"25 July 1995" , "web":"www.example.com" , "phone":"+98 918 646 7306" , "city":"Baneh" , "age":"29","degree":"Bsc" , "email":"Ashaknhpb@gmail.com"}
+    return render(request,'website/about.html' , context )
 def portfolio_view(request):
     return render(request,'website/portfolio.html')
 def portfolio_details_view(request):
